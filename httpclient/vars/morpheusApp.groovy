eae8751da137d4ed39415cb26ff4f2b0511a5186
tcook <tcooksd@gmail.com>
2018-06-07 13:55:34 -0700

    updating morpheus jenkins lib

---
+++ httpclient/vars/morpheusApp.groovy
@@ -0,0 +1,12 @@
+import org.tadamhicks.JenkinsHttpClient
+
+def buildApp(String morpheusUrl, Map<?, ?> postBody, String bearerToken) {
+	JenkinsHttpClient http = new JenkinsHttpClient()
+	http.postJson(morpheusUrl, postBody, bearerToken)
+}
+
+def pullJson(String morpheusUrl,String bearerToken) {
+	JenkinsHttpClient http = new JenkinsHttpClient()
+	http.get(morpheusUrl, bearerToken)
+}
+
